a7c84d8999f7dfe89ae4c590491e4e93e0cc36f1
texarcane <psychorussia@gmail.com>
2023-03-28 02:50:48 +0300

    Lesson 3 - complete

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,11 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+
+def user_data(name, surname, birth_year, city, email, phone):
+    return f'{name} {surname} was born in {birth_year} and lives in {city}, his email: {email}, his number: {phone}'
+
+
+print(user_data(name=input('Name: '), surname=input('Surname: '), birth_year=input('Birthday year: '),
+                city=input('City: '), email=input('Email: '), phone=input('Phone: ')))
--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,26 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+
+
+def my_func():
+    result = 0
+    stop = False
+    while not stop:
+        num_input = input('Enter numbers divided by space, to stop enter "s": ').split()
+        result1 = 0
+        for n in range(len(num_input)):
+            try:
+                if num_input[n] == 's':
+                    stop = True
+                else:
+                    result1 = result1 + int(num_input[n])
+            except ValueError:
+                print('ERROR! Wrong value!')
+                break
+        result = result + result1
+        print(f'Current sum of the entered numbers is {result}')
+    return f'Resulting sum of the entered numbers is {result}'
+
+
+print(my_func())
