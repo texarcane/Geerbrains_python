707ee37197ef8e2dc3e9de445c7733088a094975
texarcane <psychorussia@gmail.com>
2023-04-14 15:34:56 +0300

    Lesson 8 - complete

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class ZeroDivision(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+numerator = int(input('Enter numerator: '))
+denominator = int(input('Enter denominator: '))
+
+try:
+    if denominator == 0:
+        raise ZeroDivision('Dividing by zero!')
+    else:
+        result = numerator / denominator
+except ZeroDivision as err:
+    print(err)
+else:
+    print(f'Result: {numerator} / {denominator} = {result}')
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,26 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+
+class ComplexNumbers:
+    def __init__(self, value_1, value_2):
+        self.value_1 = value_1
+        self.value_2 = value_2
+
+    def __add__(self, other):
+        return ComplexNumbers(self.value_1 + other.value_1, self.value_2 + other.value_2)
+
+    def __mul__(self, other):
+        return ComplexNumbers((self.value_1 * other.value_1 - self.value_2 * other.value_2),
+                              (self.value_1 * other.value_2 + self.value_2 * other.value_1))
+
+    def __str__(self):
+        return f'{self.value_1}{"+" if self.value_2 > 0 else ""}{self.value_2}i'
+
+
+c_num_1 = ComplexNumbers(17, 76)
+c_num_2 = ComplexNumbers(18, 12)
+
+print(c_num_1+c_num_2)
+print(c_num_1*c_num_2)
